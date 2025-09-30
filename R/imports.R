28ca67e694f93baeb9e46a487b15eebed7df7e07
Chris Thaxter <chris.thaxter@bto.org>
2025-09-30 17:15:09 +0100

    Centralised imports directive for v1.1.0

---
+++ R/imports.R
@@ -0,0 +1,35 @@
+#' R/imports.R
+#' @name MoveRakeR-imports
+#' @keywords internal
+#' ------------------------------------------- #
+#' Centralised import directives for MoveRakeR
+#' ------------------------------------------- #
+#'
+#' @import dplyr
+#' @import tibble
+#' @import purrr
+#' @import tidyr
+#' @import leaflet
+#' @import shinyWidgets
+#' @import shinydashboard
+#' @import shinybusy
+#' @import units
+#' @import readr
+#' @import sf
+#' @import sp
+#' @import ggplot2
+#' @importFrom move getMovebankAnimals getMovebankLocationData movebankLogin move moveStack
+#' @importFrom RODBC sqlQuery odbcConnect
+#' @importFrom viridisLite viridis
+#' @importFrom lubridate date with_tz round_date year
+#' @importFrom geosphere distRhumb distHaversine
+#' @importFrom adehabitatLT as.ltraj redisltraj
+#' @importFrom sfheaders sf_multilinestring
+#' @importFrom htmltools tagList
+#' @importFrom suncalc getSunlightTimes
+#' @importFrom flexdashboard renderGauge
+#' @importFrom plotly ggplotly renderPlotly plotlyOutput
+#' @importFrom DT renderDataTable dataTableOutput datatable formatRound formatStyle styleEqual
+#' @rawNamespace import(shiny, except = c(dataTableOutput, renderDataTable, getQueryString))
+#' @rawNamespace import(data.table, except = c(month, hour, quarter, week, year, wday, second, minute, mday, yday, isoweek, first, last, between, transpose))
+NULL
